510c235e125343e6c65ac76808c6ed7391ab0d34
FLAMANT Thomas <32953767+syvtoto@users.noreply.github.com>
2019-02-28 19:31:01 +0100

    init

---
+++ app/src/main/java/com/example/muguet/evolutivmind/views/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.muguet.evolutivmind.views;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.example.muguet.evolutivmind.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
