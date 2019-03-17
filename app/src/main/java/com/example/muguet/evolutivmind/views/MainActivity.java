a4235547a464ca094f1f2418b2c8e302e0aa1ac4
Nhân THAN <philippethan@hotmail.com>
2019-03-17 15:48:47 +0100

    Mise en place de la seconde version du jeu

--- app/src/main/java/com/example/muguet/evolutivmind/views/MainActivity.java
+++ app/src/main/java/com/example/muguet/evolutivmind/views/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.muguet.evolutivmind.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> list;
+    private HashMap<String, Integer> listColor = new HashMap<>();
     private int correct_color;
     private int color2;
     private int color3;
@@ -27,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView img2 = (ImageView)findViewById(R.id.rectangle2);
         ImageView img3 = (ImageView)findViewById(R.id.rectangle3);
 
+        listColor.put("Bleu", Color.BLUE);
+        listColor.put("Rouge", Color.RED);
+        listColor.put("Noir", Color.BLACK);
+
+        list = new ArrayList<String>(listColor.keySet());
+
         newGame();
         verif(img, img2, img3);
 
@@ -37,23 +50,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_LONG).show();
-                newGame();
+                changeGame();
             }
         });
 
         (img2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "NO", Toast.LENGTH_LONG).show();
-                newGame();
+                Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_LONG).show();
+                changeGame();
             }
         });
 
         (img3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "NO", Toast.LENGTH_LONG).show();
-                newGame();
+                Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_LONG).show();
+                changeGame();
             }
         });
     }
@@ -61,6 +74,12 @@ public class MainActivity extends AppCompatActivity {
     private int setRandomColor(){
         Random rnd = new Random();
         return Color.argb(255, rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
+    }
+
+    private String setRandomColorString(){
+        Random random = new Random();
+        String randomColor = list.get(random.nextInt(list.size()));
+        return randomColor;
     }
 
     private void switchPosition(ImageView img,ImageView img2,ImageView img3){
@@ -132,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
         color3 = setRandomColor();
 
         mot.setTextColor(correct_color);
+        mot.setText("Reset");
 
         drawable.setColorFilter(correct_color, PorterDuff.Mode.SRC_ATOP);
         img.setBackground(drawable);
@@ -146,4 +166,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void newGame2(){
+
+        TextView mot = (TextView) findViewById(R.id.mot);
+        ImageView img = (ImageView)findViewById(R.id.rectangle);
+        ImageView img2 = (ImageView)findViewById(R.id.rectangle2);
+        ImageView img3 = (ImageView)findViewById(R.id.rectangle3);
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.rectangle);
+        Drawable drawable2 = res.getDrawable(R.drawable.rectangle2);
+        Drawable drawable3 = res.getDrawable(R.drawable.rectangle3);
+        String rndColorString = setRandomColorString();
+
+        correct_color = listColor.get(rndColorString);
+        color2 = setRandomColor();
+        color3 = setRandomColor();
+
+        mot.setTextColor(color2);
+        mot.setText(rndColorString);
+
+        drawable.setColorFilter(correct_color, PorterDuff.Mode.SRC_ATOP);
+        img.setBackground(drawable);
+
+        drawable2.setColorFilter(color2, PorterDuff.Mode.SRC_ATOP);
+        img2.setBackground(drawable2);
+
+        drawable3.setColorFilter(color3, PorterDuff.Mode.SRC_ATOP);
+        img3.setBackground(drawable3);
+
+        switchPosition(img, img2, img3);
+
+    }
+
+    private void changeGame(){
+        Random rnd = new Random();
+        int game = rnd.nextInt(2);
+        Log.d("Couleur ", Integer.toString(game));
+        if(game == 1){
+            newGame();
+        }else{
+            newGame2();
+        }
+    }
 }
