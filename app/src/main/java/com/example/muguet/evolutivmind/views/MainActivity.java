adb34ac9d0d017917efd284a1d70e231ccdb0d58
Nhân THAN <philippethan@hotmail.com>
2019-04-01 23:35:57 +0200

    Merge interfaces

--- app/src/main/java/com/example/muguet/evolutivmind/views/MainActivity.java
+++ app/src/main/java/com/example/muguet/evolutivmind/views/MainActivity.java
@@ -3,10 +3,10 @@ package com.example.muguet.evolutivmind.views;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
 import com.example.muguet.evolutivmind.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b_play = findViewById(R.id.button_play);
+        Button b_play = findViewById(R.id.btnJouer);
         b_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
