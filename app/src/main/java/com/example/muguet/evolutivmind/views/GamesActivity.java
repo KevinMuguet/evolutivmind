b47204ff924e937e48f4f6f09ee552ea08e1a1d5
Nhân THAN <philippethan@hotmail.com>
2019-04-02 00:20:20 +0200

    Ajout colorwords

--- app/src/main/java/com/example/muguet/evolutivmind/views/GamesActivity.java
+++ app/src/main/java/com/example/muguet/evolutivmind/views/GamesActivity.java
@@ -1,6 +1,9 @@
 package com.example.muguet.evolutivmind.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.muguet.evolutivmind.R;
 
@@ -10,6 +13,14 @@ public class GamesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+        Button b_colorwords = findViewById(R.id.button_colorwords);
+        b_colorwords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ColorWordsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
