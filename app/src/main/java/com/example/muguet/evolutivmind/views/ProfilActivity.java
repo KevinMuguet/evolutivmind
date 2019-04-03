c8830e3ef33faf28615bdeeaa2f4fe1f9560a46a
FLAMANT Thomas <32953767+syvtoto@users.noreply.github.com>
2019-04-03 15:55:44 +0200

    layout corrigée

---
+++ app/src/main/java/com/example/muguet/evolutivmind/views/ProfilActivity.java
@@ -0,0 +1,14 @@
+package com.example.muguet.evolutivmind.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.muguet.evolutivmind.R;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
