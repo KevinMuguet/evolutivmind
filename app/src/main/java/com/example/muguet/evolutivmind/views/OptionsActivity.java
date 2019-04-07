91d898dc4fb40d2b5a0ae405b5b0c3c508b45ac5
FLAMANT Thomas <32953767+syvtoto@users.noreply.github.com>
2019-04-07 16:56:47 +0200

    lottie animation arretée après une loop

--- app/src/main/java/com/example/muguet/evolutivmind/views/OptionsActivity.java
+++ app/src/main/java/com/example/muguet/evolutivmind/views/OptionsActivity.java
@@ -1,12 +1,15 @@
 package com.example.muguet.evolutivmind.views;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.muguet.evolutivmind.R;
+import android.util.Log;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -19,6 +22,31 @@ public class OptionsActivity extends AppCompatActivity {
 
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
         final SharedPreferences.Editor editor = pref.edit();
+
+        final LottieAnimationView lottieAnimationView = findViewById(R.id.animation_view_options);
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+//                Log.d("debugPerso","start");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+//                Log.d("debugPerso","end");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+//                Log.d("debugPerso","cancel");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                lottieAnimationView.pauseAnimation();
+//                Log.d("debugPerso","repeat");
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
